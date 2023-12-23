5dc19018248cf8c601fe9848900ff0ff72f177d3
Nabil Kachar <kacharnabil1@gmail.com>
2023-12-23 12:16:18 +0100

    adding app routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './pages/Login'
+import Signup from "./pages/Signup";
+import Chat from "./pages/Chat";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+    <Routes>
+     <Route path="/login" element={ <Login/>} />
+     <Route path="/register" element={ <Signup/>} />
+     <Route path="/chat" element={ <Chat/>} />
+     <Route path="/" element={ <Home/>} />
+     <Route path="*" element={ <NotFound/>} />
+    </Routes>
+    </BrowserRouter>
   )
 }
