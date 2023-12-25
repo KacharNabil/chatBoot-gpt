70a8a4d417cf829d727c706149e4ed2befd072f6
Nabil Kachar <kacharnabil1@gmail.com>
2023-12-25 10:13:48 +0100

    ui login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import { Input } from "@nextui-org/react";
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+        </div>
+        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form className="card-body">
+            <div className="form-control">
+              <Input
+                isRequired
+                type="email"
+                label="Email"
+                className="max-w-xs bg-transparent"
+                variant="bordered"
+              />
+            </div>
+            <div className="form-control mt-8">
+            <Input
+                isRequired
+                type="password"
+                label="Password"
+                className="max-w-xs bg-transparent"
+                variant="bordered"
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -12,6 +12,9 @@ export default {
     extend: {},
   },
   darkMode: "class",
-  plugins: [nextui()]
+  plugins: [nextui(),require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
 }
 
